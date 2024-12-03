25af72203a4a915aef20b5b041e508ab2fbab49d
Y Makarevich <y.makarevich@lumc.nl>
2024-12-03 16:23:44 +0100

    add December 3rd part 2

--- 2024/03/2.py
+++ 2024/03/2.py
@@ -1,3 +1,18 @@
+import re
+
+
 if __name__ == "__main__":
-    with open("0/input.txt", "r") as f:
-        pass
+    with open("03/input.txt", "r") as f:
+        program = f.read()
+
+    do_splits = program.split("do()")
+    program = "".join([s.split("don't")[0] for s in do_splits])
+
+    pattern = re.compile(r"mul\((\d{1,3}),(\d{1,3})\)")
+    matches = pattern.findall(program)
+
+    result = 0
+    for i in range(len(matches)):
+        result += int(matches[i][0]) * int(matches[i][1])
+
+    print(result)
