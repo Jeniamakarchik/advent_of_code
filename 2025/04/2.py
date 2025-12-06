24e90c5069000a67aa2aaf1b40ed6399072cab21
Jenia Makarevich <y.makarevich@lumc.nl>
2025-12-06 20:15:02 +0100

    add 2025-12-04, both parts

---
+++ 2025/04/2.py
@@ -0,0 +1,51 @@
+import numpy as np
+
+
+def is_valid_pos(pos, grid):
+    x,y = pos
+    if x < 0 or y < 0:
+        return False
+    try:
+        grid[x][y]
+        return True
+    except IndexError:
+        return False
+
+
+def check_symbol(grid, coord):
+    x, y = coord
+    offsets = [(-1, -1), (-1, 0), (-1, 1), 
+              (0, -1),          (0, 1),
+              (1, -1), (1, 0), (1, 1)]
+
+    rolls_nearby = 0
+    for offset in offsets:
+        if is_valid_pos((x + offset[0], y + offset[1]), grid):
+            x_, y_ = x + offset[0], y + offset[1]
+            if grid[x_][y_] == "@":
+                rolls_nearby += 1
+    
+    return rolls_nearby < 4
+
+
+if __name__ == "__main__":
+    with open("2025/04/input.txt", "r") as f:
+        grid = [list(line.strip()) for line in f.readlines()]
+    
+    total = 0
+    positions = []
+    can_be_removed = np.inf
+    while can_be_removed > 0:
+        can_be_removed = 0
+        for ind, line in enumerate(grid):
+            for jnd, el in enumerate(line):
+                if el == ".":
+                    continue
+
+                if check_symbol(grid, (ind, jnd)):
+                    positions.append((ind, jnd))
+                    grid[ind][jnd] = "."
+                    can_be_removed += 1
+        total += can_be_removed
+
+    print(total)
